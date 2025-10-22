870d42fc4d48080e74e719a5fc33888ed9276519
tomydiaz <tomydiaz739@gmail.com>
2025-10-21 22:59:51 -0300

    forma ingenua arreglada con mas ejemplos

--- ejercicio1.ts
+++ ejercicio1.ts
@@ -10,26 +10,30 @@ const ejemplo1: Punto[] = [
   { x: 22, y: 22 },
   { x: -22, y: -22 },
 ];
+const ejemplo2: Punto[] = generarListaDePuntosAleatorios(50, 50);
+const ejemplo3: Punto[] = generarListaDePuntosAleatorios(500, 500);
+const ejemplo4: Punto[] = generarListaDePuntosAleatorios(5000, 5000);
+const ejemplo5: Punto[] = generarListaDePuntosAleatorios(20000, 20000);
 
-// Funcion para encontrar los dos puntos mas alejados - FORMA INGENUA
-function obtenerPuntosMasAlejadosIngenua(puntos: Punto[]): { puntosMasAlejados: Punto[]; tiempoDeEjecucion: number } {
-  if (puntos.length < 2) {
-    return { puntosMasAlejados: [], tiempoDeEjecucion: 0 };
+// Funcion para encontrar los dos puntos mas cercanos - FORMA INGENUA
+function obtenerPuntosMasCercanosIngenua(puntos: Punto[]): { puntosMasCercanos: Punto[]; tiempoDeEjecucion: number } {
+  if (puntos.length <= 2) {
+    return { puntosMasCercanos: puntos, tiempoDeEjecucion: 0 };
   }
 
   const startTime = performance.now();
 
-  let maxDistancia = 0;
-  let puntosMasAlejados: Punto[] = [];
+  let minDistancia = Infinity;
+  let puntosMasCercanos: Punto[] = [];
 
   for (let i = 0; i < puntos.length; i++) {
     for (let j = i + 1; j < puntos.length; j++) {
       const dx = puntos[i].x - puntos[j].x;
       const dy = puntos[i].y - puntos[j].y;
       const distancia = Math.sqrt(dx * dx + dy * dy);
-      if (distancia > maxDistancia) {
-        maxDistancia = distancia;
-        puntosMasAlejados = [puntos[i], puntos[j]];
+      if (distancia < minDistancia) {
+        minDistancia = distancia;
+        puntosMasCercanos = [puntos[i], puntos[j]];
       }
     }
   }
@@ -38,16 +42,36 @@ function obtenerPuntosMasAlejadosIngenua(puntos: Punto[]): { puntosMasAlejados: 
 
   const tiempoDeEjecucion = endTime - startTime;
 
-  return { puntosMasAlejados, tiempoDeEjecucion };
+  return { puntosMasCercanos, tiempoDeEjecucion };
 }
 
-const ejemplo1Ingenua = obtenerPuntosMasAlejadosIngenua(ejemplo1);
+const ejemplo1Ingenua = obtenerPuntosMasCercanosIngenua(ejemplo1);
 
-console.log('Ejemplo 1 - Forma Ingenua - Resultado  - ', ejemplo1.length, 'puntos:', ejemplo1Ingenua.puntosMasAlejados);
+console.log('Ejemplo 1 - Forma Ingenua - Resultado  - ', ejemplo1.length, 'puntos:', ejemplo1Ingenua.puntosMasCercanos);
 console.log('Ejemplo 1 - Forma Ingenua - Tiempo de ejecucion - ', ejemplo1.length, 'puntos:', ejemplo1Ingenua.tiempoDeEjecucion);
 
-// Funcion para encontrar los dos puntos mas alejados - DIVIDE AND CONQUER
-function obtenerPuntosMasAlejadosDivideAndConquer(puntos: Punto[]): { puntosMasAlejados: Punto[]; tiempoDeEjecucion: number } {
+const ejemplo2Ingenua = obtenerPuntosMasCercanosIngenua(ejemplo2);
+
+console.log('Ejemplo 2 - Forma Ingenua - Resultado  - ', ejemplo2.length, 'puntos:', ejemplo2Ingenua.puntosMasCercanos);
+console.log('Ejemplo 2 - Forma Ingenua - Tiempo de ejecucion - ', ejemplo2.length, 'puntos:', ejemplo2Ingenua.tiempoDeEjecucion);
+
+const ejemplo3Ingenua = obtenerPuntosMasCercanosIngenua(ejemplo3);
+
+console.log('Ejemplo 3 - Forma Ingenua - Resultado  - ', ejemplo3.length, 'puntos:', ejemplo3Ingenua.puntosMasCercanos);
+console.log('Ejemplo 3 - Forma Ingenua - Tiempo de ejecucion - ', ejemplo3.length, 'puntos:', ejemplo3Ingenua.tiempoDeEjecucion);
+
+const ejemplo4Ingenua = obtenerPuntosMasCercanosIngenua(ejemplo4);
+
+console.log('Ejemplo 4 - Forma Ingenua - Resultado  - ', ejemplo4.length, 'puntos:', ejemplo4Ingenua.puntosMasCercanos);
+console.log('Ejemplo 4 - Forma Ingenua - Tiempo de ejecucion - ', ejemplo4.length, 'puntos:', ejemplo4Ingenua.tiempoDeEjecucion);
+
+const ejemplo5Ingenua = obtenerPuntosMasCercanosIngenua(ejemplo5);
+
+console.log('Ejemplo 5 - Forma Ingenua - Resultado  - ', ejemplo5.length, 'puntos:', ejemplo5Ingenua.puntosMasCercanos);
+console.log('Ejemplo 5 - Forma Ingenua - Tiempo de ejecucion - ', ejemplo5.length, 'puntos:', ejemplo5Ingenua.tiempoDeEjecucion);
+
+// Funcion para encontrar los dos puntos mas cercanos - DIVIDE AND CONQUER
+function obtenerPuntosMasCercanosDivideAndConquer(puntos: Punto[]): { puntosMasCercanos: Punto[]; tiempoDeEjecucion: number } {
   const startTime = performance.now();
 
   // algoritmo divide and conquer aca
@@ -56,5 +80,15 @@ function obtenerPuntosMasAlejadosDivideAndConquer(puntos: Punto[]): { puntosMasA
 
   const tiempoDeEjecucion = endTime - startTime;
 
-  return { puntosMasAlejados: [], tiempoDeEjecucion };
+  return { puntosMasCercanos: [], tiempoDeEjecucion };
+}
+
+function generarListaDePuntosAleatorios(cantidad: number, rango: number): Punto[] {
+  const puntos: Punto[] = [];
+  for (let i = 0; i < cantidad; i++) {
+    const x = Math.floor(Math.random() * rango * 2) - rango;
+    const y = Math.floor(Math.random() * rango * 2) - rango;
+    puntos.push({ x, y });
+  }
+  return puntos;
 }
