5f155e1e52362d3cfa12f14c5ed8cf21929b3980
tomydiaz <tomydiaz739@gmail.com>
2025-10-23 23:52:24 -0300

    ejemplos

--- ejercicio2.ts
+++ ejercicio2.ts
@@ -6,13 +6,19 @@ type Actividad = {
 };
 
 const ejemplo1: Actividad[] = [
-  { inicio: 1, fin: 3 },
-  { inicio: 2, fin: 4 },
-  { inicio: 3, fin: 5 },
-  { inicio: 0, fin: 6 },
-  { inicio: 5, fin: 7 },
-];
+  { inicio: 1, fin: 2 },
+  { inicio: 3, fin: 4 },
+  { inicio: 5, fin: 6 },
+  { inicio: 7, fin: 8 },
+  { inicio: 8, fin: 10 },
+]; // Respuesta esperada: 4 actividades no solapadas
 
+const ejemplo2: Actividad[] = generarListaDeActividadesAleatorias(50, 50);
+const ejemplo3: Actividad[] = generarListaDeActividadesAleatorias(500, 500);
+const ejemplo4: Actividad[] = generarListaDeActividadesAleatorias(5000, 5000);
+const ejemplo5: Actividad[] = generarListaDeActividadesAleatorias(20000, 20000);
+
+// Funcion para encontrar la mayor cantidad de actividades no solapadas - FORMA GREEDY
 function actividades(actividades: Actividad[]): Actividad[] {
   const actividadesOrdenadas = actividades.sort((a, b) => a.fin - b.fin);
   const S: Actividad[] = [];
@@ -28,46 +34,52 @@ function actividades(actividades: Actividad[]): Actividad[] {
   return S;
 }
 
-const ejemplo1Ingenua = obtenerPuntosMasCercanosIngenua(ejemplo1);
+//
+
+const startTimeEjemplo1 = performance.now();
+const ejemplo1Greedy = actividades(ejemplo1);
+const endTimeEjemplo1 = performance.now();
 const tiempoDeEjecucionEjemplo1 = endTimeEjemplo1 - startTimeEjemplo1;
-console.log('Ejemplo 1 - Forma Ingenua - Resultado  - ', ejemplo1.length, 'puntos:', ejemplo1Ingenua);
-console.log('Ejemplo 1 - Forma Ingenua - Tiempo de ejecucion - ', ejemplo1.length, 'puntos:', tiempoDeEjecucionEjemplo1);
+console.log('Ejemplo 1 - Forma Greedy - Resultado  - ', ejemplo1.length, 'actividades - cantidad de no solapadas:', ejemplo1Greedy.length);
+console.log('Ejemplo 1 - Forma Greedy - Tiempo de ejecucion - ', ejemplo1.length, 'actividades:', tiempoDeEjecucionEjemplo1);
 
 //
 
 const startTimeEjemplo2 = performance.now();
-const ejemplo2Ingenua = obtenerPuntosMasCercanosIngenua(ejemplo2);
+const ejemplo2Greedy = actividades(ejemplo2);
 const endTimeEjemplo2 = performance.now();
 const tiempoDeEjecucionEjemplo2 = endTimeEjemplo2 - startTimeEjemplo2;
-console.log('Ejemplo 2 - Forma Ingenua - Resultado  - ', ejemplo2.length, 'puntos:', ejemplo2Ingenua);
-console.log('Ejemplo 2 - Forma Ingenua - Tiempo de ejecucion - ', ejemplo2.length, 'puntos:', tiempoDeEjecucionEjemplo2);
+console.log('Ejemplo 2 - Forma Greedy - Resultado  - ', ejemplo2.length, 'actividades - cantidad de no solapadas:', ejemplo2Greedy.length);
+console.log('Ejemplo 2 - Forma Greedy - Tiempo de ejecucion - ', ejemplo2.length, 'actividades:', tiempoDeEjecucionEjemplo2);
 
 //
 
 const startTimeEjemplo3 = performance.now();
-const ejemplo3Ingenua = obtenerPuntosMasCercanosIngenua(ejemplo3);
+const ejemplo3Greedy = actividades(ejemplo3);
 const endTimeEjemplo3 = performance.now();
 const tiempoDeEjecucionEjemplo3 = endTimeEjemplo3 - startTimeEjemplo3;
-console.log('Ejemplo 3 - Forma Ingenua - Resultado  - ', ejemplo3.length, 'puntos:', ejemplo3Ingenua);
-console.log('Ejemplo 3 - Forma Ingenua - Tiempo de ejecucion - ', ejemplo3.length, 'puntos:', tiempoDeEjecucionEjemplo3);
+console.log('Ejemplo 3 - Forma Greedy - Resultado  - ', ejemplo3.length, 'actividades - cantidad de no solapadas:', ejemplo3Greedy.length);
+console.log('Ejemplo 3 - Forma Greedy - Tiempo de ejecucion - ', ejemplo3.length, 'actividades:', tiempoDeEjecucionEjemplo3);
 
 //
 
 const startTimeEjemplo4 = performance.now();
-const ejemplo4Ingenua = obtenerPuntosMasCercanosIngenua(ejemplo4);
+const ejemplo4Greedy = actividades(ejemplo4);
 const endTimeEjemplo4 = performance.now();
 const tiempoDeEjecucionEjemplo4 = endTimeEjemplo4 - startTimeEjemplo4;
-console.log('Ejemplo 4 - Forma Ingenua - Resultado  - ', ejemplo4.length, 'puntos:', ejemplo4Ingenua);
-console.log('Ejemplo 4 - Forma Ingenua - Tiempo de ejecucion - ', ejemplo4.length, 'puntos:', tiempoDeEjecucionEjemplo4);
+console.log('Ejemplo 4 - Forma Greedy - Resultado  - ', ejemplo4.length, 'actividades - cantidad de no solapadas:', ejemplo4Greedy.length);
+console.log('Ejemplo 4 - Forma Greedy - Tiempo de ejecucion - ', ejemplo4.length, 'actividades:', tiempoDeEjecucionEjemplo4);
 
 //
 
 const startTimeEjemplo5 = performance.now();
-const ejemplo5Ingenua = obtenerPuntosMasCercanosIngenua(ejemplo5);
+const ejemplo5Greedy = actividades(ejemplo5);
 const endTimeEjemplo5 = performance.now();
 const tiempoDeEjecucionEjemplo5 = endTimeEjemplo5 - startTimeEjemplo5;
-console.log('Ejemplo 5 - Forma Ingenua - Resultado  - ', ejemplo5.length, 'puntos:', ejemplo5Ingenua);
-console.log('Ejemplo 5 - Forma Ingenua - Tiempo de ejecucion - ', ejemplo5.length, 'puntos:', tiempoDeEjecucionEjemplo5);
+console.log('Ejemplo 5 - Forma Greedy - Resultado  - ', ejemplo5.length, 'actividades - cantidad de no solapadas:', ejemplo5Greedy.length);
+console.log('Ejemplo 5 - Forma Greedy - Tiempo de ejecucion - ', ejemplo5.length, 'actividades:', tiempoDeEjecucionEjemplo5);
+
+//
 
 function generarListaDeActividadesAleatorias(cantidad: number, rango: number): Actividad[] {
   const actividades: Actividad[] = [];
